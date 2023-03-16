b8a6f83db566213d7c1fb2b9037a07c99ad930c5
Chirag A K <chiragajekar@gmail.com>
2023-03-16 23:02:28 +0530

    .

--- index.php
+++ index.php
@@ -141,6 +141,24 @@ margin-left:12%;
 color:white;
 margin-bottom:12%;
 }
+#contacts{
+/* background-color:green; */
+height:auto;
+margin-top:0px;
+position:relative;
+z-index:1;
+}
+.contact {
+/* background-color: brown; */
+width: 78%;
+height:auto;
+display:flex;
+align-items:center;
+margin-left:12%;
+color:white;
+margin-bottom:12%;
+
+}
 
 .heading h1 {
   white-space: nowrap;
@@ -153,6 +171,136 @@ margin-bottom:12%;
   color:white;
   font-size:25px;
 }
+
+
+/* ------------------------------------------------------- */
+form{
+   width:100%;
+   margin-top:15%;
+}
+.container {
+  width: 80%;
+  padding: 0 20px;
+  align-items: center;
+  /* margin-top:15%; */
+}
+
+.container h2 {
+  width: 100%;
+  color: #04C3C3;
+  font-size: 36px;
+  text-align: center;
+  margin-bottom: 10px;
+  position: fixed;
+}
+
+.container .row100 {
+  width: 100%;
+  display: grid;
+
+}
+
+.container .row100 .col {
+  position: relative;
+  width: 80%;
+  padding: 0 10px;
+  margin: 30px 50px 10px;
+  transition: 0.5s;
+}
+
+.container .row100 .inputBox {
+  position: relative;
+  width: 100%;
+  height: 40px;
+  color: #04C3C3;
+}
+
+.container .row100 .inputBox input,
+.container .row100 .inputBox.textarea textarea {
+  position: absolute;
+  width: 98%;
+  height: 100%;
+  background: transparent;
+  box-shadow: none;
+  border: none;
+  outline: none;
+  font-size: 18px;
+  padding: 0 10px;
+  z-index: 1;
+  color: #000;
+}
+
+.container .row100 .inputBox .text {
+  position: absolute;
+  top: 0;
+  left: 0;
+  line-height: 40px;
+  font-size: 18px;
+  padding: 0 10px;
+  display: block;
+  transition: 0.5s;
+  pointer-events: none;
+}
+
+.container .row100 .inputBox input:focus + .text,
+.container .row100 .inputBox input:valid + .text {
+  top: -35px;
+  left: -10px;
+}
+
+.container .row100 .inputBox .line {
+  position: absolute;
+  bottom: 0;
+  display: block;
+  width: 100%;
+  height: 2px;
+  background: #04C3C3;
+  transition: 0.5s;
+  border-radius: 2px;
+  pointer-events: none;
+}
+
+.container .row100 .inputBox input:focus ~ .line,
+.container .row100 .inputBox input:valid ~ .line {
+  height: 100%;
+}
+
+.container .row100 .inputBox.textarea {
+  position: relative;
+  width: 100%;
+  height: 100px;
+  padding: 10px 0;
+}
+
+.container .row100 .inputBox.textarea textarea {
+  height: 100%;
+  resize: none;
+}
+
+.container .row100 .inputBox textarea:focus + .text,
+.container .row100 .inputBox textarea:valid + .text {
+  top: -35px;
+  left: -10px;
+}
+
+.container .row100 .inputBox textarea:focus ~ .line,
+.container .row100 .inputBox textarea:valid ~ .line {
+  height: 100%;
+  
+}
+
+input[type="submit"] {
+  border: none;
+  padding: 7px 35px;
+  cursor: pointer;
+  outline: none;
+  background: #04C3C3;
+  color: #000;
+  font-size: 18px;
+  border-radius: 2px;
+}
+
+
       </style>
    </head>
    <body >
@@ -244,14 +392,69 @@ margin-bottom:12%;
           </p>
         </div>
       </section>
+
+      <section id="contacts">
+        <div class="contact">
+        <div class="heading">
+			<h1>contact</h1>
+			<div class="straight-line"></div>          
+        </div>
+        <form action="https://formsubmit.co/el/ducebi" method="POST">
+        <div class="container">
+      <div class="row100">
+        <div class="col">
+          <div class="inputBox">
+            <input type="text" name="" required="required">
+            <span class="text">First Name</span>
+            <span class="line"></span>
+          </div>
+        </div>
+        <div class="col">
+          <div class="inputBox">
+            <input type="text" name="" required="required">
+            <span class="text">Last Name</span>
+            <span class="line"></span>
+          </div>
+        </div>
+      </div>
+      <div class="row100">
+        <div class="col">
+          <div class="inputBox">
+            <input type="text" name="" required="required">
+            <span class="text">Email</span>
+            <span class="line"></span>
+          </div>
+        </div>
+       
+      <div class="row100">
+        <div class="col">
+          <div class="inputBox textarea">
+            <textarea required="required"></textarea>
+            <span class="text">Type your message Here...</span>
+            <span class="line"></span>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row100">
+        <div class="col">
+          <input type="submit" value="Send">
+        </div>
+      </div>
+    </div>
+    </form>
+       
+       
+        </div>
+      </section>
       
 
-      
-
-      
-    </script>
    </body>
 </html>
 
+
+      
+
+      
 
 
